e9173fe16ce64ffa574d33217e46baa654ac36ab
Jaston1026 <yizhong1026@gmail.com>
2021-07-07 19:56:20 +0800

    Update start.js

--- src/components/start.js
+++ src/components/start.js
@@ -5,6 +5,9 @@ const model = require("./models/model.js")
 module.exports = {
 	key: "start",
 	func: async (message, args, client, commands) => {
-		console.log(model.profile.find())
+		model.profile.find({uid: `acc${message.author.id}`}, (err, docs) => {
+			console.log(err)
+			console.log(docs)
+		})
 	}
 }
