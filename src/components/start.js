a07e955d3a4daaa831634b71ad01e519de22de01
Jaston1026 <yizhong1026@gmail.com>
2021-07-07 20:03:54 +0800

    Update start.js

--- src/components/start.js
+++ src/components/start.js
@@ -5,7 +5,7 @@ const model = require("./models/model.js")
 module.exports = {
 	key: "start",
 	func: async (message, args, client, commands) => {
-		if(model.profile.find({uid: `acc${message.author.id}`}) === null) {
+		if(model.profile.find({uid: `acc${message.author.id}`}).exec() === null) {
 			message.channel.send(`No profile found for ${message.author.id}`)
 		}
 	}
