b5dcb5730ddde631c7f52ee0ebba278460cc9f9f
Jaston1026 <yizhong1026@gmail.com>
2021-07-08 11:03:37 +0800

    Update bot.js

--- src/bot.js
+++ src/bot.js
@@ -116,7 +116,7 @@ mongoose.connect(config.table[0].MONGODB_URI, {
 	useUnifiedTopology: true
 }) .then(() => {
 	console.log("Database Connected")
-	client.channel.cache.get("861187530338336788").send("Bot Restarted")
+	client.channels.cache.get("861187530338336788").send("Bot Restarted")
 })
 
 //bot initial running code similar to setup
