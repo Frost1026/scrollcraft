01a1a7f3a4ec06fd0c55180ed6e0ce36239c620c
Jaston1026 <yizhong1026@gmail.com>
2021-07-09 16:54:06 +0800

    Update start.js

--- src/components/start.js
+++ src/components/start.js
@@ -11,12 +11,14 @@ module.exports = {
 		model.profile.find({uid: `acc${message.author.id}`}, (err, docs) => {
 			if(err == null) {
 				startingOutEmbed
-				.setAuthor(message.author.tag, message.author.displayAvatarURL({ format: "png", dynamic: true }))
-				.setTitle("Character Creation")
-				.setDescription("Everyone only have a chance at character creation, so choose your options wisely.")
-				.addField("React on the arrows to scroll through available classes")
-				.setFooter("Classes will only determine your initial stats and initial ability to master which weapon.")
-				.setTimestamp()
+					.setAuthor(message.author.tag, message.author.displayAvatarURL({ format: "png", dynamic: true }))
+					.setTitle("Character Creation")
+					.setDescription("Everyone only have a chance at character creation, so choose your options wisely.")
+					.addField("React on the arrows to scroll through available classes")
+					.setFooter("Classes will only determine your initial stats and initial ability to master which weapon.")
+					.setTimestamp()
+
+				message.channel.send(startingOutEmbed)
 			} else {
 				message.channel.send(`**${message.author} already have a profile on the database**`)
 			}
