4ac1f67d7da8c0603dbc796eebc52548244482eb
Jaston1026 <yizhong1026@gmail.com>
2021-07-16 15:21:22 +0800

    Update start.js

--- src/components/start.js
+++ src/components/start.js
@@ -101,6 +101,7 @@ module.exports = {
 
 					list.awaitReactions(confirmationFilter, {max: 1, time: 30000, errors: ['time']}).then((collected) => {
 						console.log(collected.name)
+						
 						if(collected.emoji.name === '✅') {
 							list.edit(generateEmbed(1))
 							// message.channel.send(generateEmbed(1)).then((list_2) => {
