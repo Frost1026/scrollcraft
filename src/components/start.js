bb29b0a76747c54db6e8c11f57f3d2d5d5e516d3
Jaston1026 <yizhong1026@gmail.com>
2021-07-16 15:28:49 +0800

    Update start.js

--- src/components/start.js
+++ src/components/start.js
@@ -100,7 +100,7 @@ module.exports = {
 					}
 
 					list.awaitReactions(confirmationFilter, {max: 1, time: 30000, errors: ['time']}).then((collected) => {
-						console.log(collected.reaction)
+						console.log(collected.first().reaction.emoji.name)
 
 						if(collected.emoji.name === '✅') {
 							list.edit(generateEmbed(1))
