c23aa4f78ad27a72b2c3a0bd9c53786846916238
Jaston1026 <yizhong1026@gmail.com>
2021-07-17 22:20:33 +0800

    Update button.js

--- src/components/libraries/button.js
+++ src/components/libraries/button.js
@@ -12,46 +12,34 @@ module.exports = {
             ..._options
         }
 
-        console.log(options)
+        console.log(Object.entries(options))
 
         const button = new discordButton.MessageButton()
-
-        console.log(!!options.emoji)
 
         Object.entries(options).forEach(value => {
             switch(true) {
                 case !!options.id:
-                    console.log("ID exists")
                     options.id = null
                     break
 
                 case !!options.style:
-                    console.log("Style exists")
                     options.style = null
                     break
 
                 case !!options.label:
-                    console.log("Label exists")
                     options.label = null
                     break
 
                 case !!options.emoji:
-                    console.log("Emoji exists")
                     options.emoji = null
                     break
 
                 case !!options.emojiAnimated:
-                    console.log("Emoji Animated")
                     options.emojiAnimated = null
                     break
 
                 case !!options.url:
-                    console.log("URL exists")
                     options.url = null
-                    break
-
-                default:
-                    console.log("All parameters null")
                     break
             }
         })
