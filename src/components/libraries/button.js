5a00bda3bf5eb9fc81cca046491ab0d6ec65fc44
Jaston1026 <yizhong1026@gmail.com>
2021-07-17 20:17:48 +0800

    Update button.js

--- src/components/libraries/button.js
+++ src/components/libraries/button.js
@@ -1,12 +1,12 @@
 const button = require("discord-buttons")
 
 const buttonParameters = {
-    id,
-    style,
-    label,
-    emoji,
-    emojiAnimated,
-    url,
+    id: {type: String},
+    style: {type: String},
+    label: {type: String},
+    emoji: {type: String},
+    emojiAnimated: {type: Boolean},
+    url: {type: String},
 }
 
 module.exports = {
