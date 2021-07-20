f53bb63d81c007b3016a856ad35840b942c55952
Jaston1026 <yizhong1026@gmail.com>
2021-07-20 13:51:20 +0800

    Update start.js

--- src/components/start.js
+++ src/components/start.js
@@ -36,11 +36,7 @@ module.exports = {
 	desc: "Creates player profile for the game",
 	func: async (message, args, client, commands) => {
 		model.profile.find({uid: `acc_${message.author.id}`}, (err, docs) => {
-			if(docs.length) {
-				console.log("Document exists")
-			}
-			console.log(err)
-			if(err == null) {
+			if(!docs.length) {
 				refreshJSONBuffer(classes_file, classes)
 
 				let payload
