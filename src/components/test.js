631a1af770f3b08a74243ea63e4b407bc04e3c3a
Jaston1026 <yizhong1026@gmail.com>
2021-08-23 20:44:29 +0800

    Initial commit from new pc

--- src/components/test.js
+++ src/components/test.js
@@ -6,26 +6,13 @@ const discordButton = require("./libraries/button.js")
 const button = require("./libraries/button.js")
 
 module.exports = {
-    key: "test",
+	key: "test",
     desc: "Testing commands and code in development 🛠",
     func: async (message, args, client, commands, config) => {
         if(config.table[0].WHITELIST.includes(message.author.username)){
             model.profile.find({uid: `acc_${message.author.id}`}, (err, docs) => {
                 if(docs) {
-                    const player = docs[0]
-
-                    console.log(typeof player.username)
-                    // console.log(client.users.cache.find(user => user.username === player.username).id)
-
-                    const damageCalculator = (stats = {}) => {
-                        
-                    }
-        
-                    switch(args[0]) {
-                        case "damage":
-                            damageCalculator()
-                            break
-                    }
+                    
                 } else {
                     message.channel.send(`**No account found for <@${player.username}>**`)
                 }
